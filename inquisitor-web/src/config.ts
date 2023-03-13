61f22e4d552aa13cd2a7a7667328b469bd775959
Peter Fajner <peter@pfaj.ca>
2023-03-12 21:54:28 -0300

    Add talents and exotic abilities URLs

--- inquisitor-web/src/config.ts
+++ inquisitor-web/src/config.ts
@@ -1,6 +1,8 @@
 const config = {
     sheets: {
         stats: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR31iDYvOZXmFttcQ2Nzk31q1TjhUuW4lquWcOTKKiGdXXesRFcsimj77ejO82SORzg8-X4Zaz7B9KN/pub?gid=0&single=true&output=csv",
+        talents: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR31iDYvOZXmFttcQ2Nzk31q1TjhUuW4lquWcOTKKiGdXXesRFcsimj77ejO82SORzg8-X4Zaz7B9KN/pub?gid=1801499678&single=true&output=csv",
+        exotic_abilities: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR31iDYvOZXmFttcQ2Nzk31q1TjhUuW4lquWcOTKKiGdXXesRFcsimj77ejO82SORzg8-X4Zaz7B9KN/pub?gid=1452317687&single=true&output=csv",
     },
 };
 
